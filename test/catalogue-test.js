367de300492577aa87f11a0006a5302131762efb
Ruoye Qian <20095880@mail.wit.ie>
2021-10-19 13:07:27 +0100

    batchAdd feature developed.

--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -10,7 +10,7 @@ describe("Catalogue", () => {
   beforeEach( () => {
     cat = new Catalogue("Test Catalogue");
     cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0));
-    cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
+    cat.addProduct(new Product("A124", "Product 2", 100, 10, 10.0));
     cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
   });
   describe("findProductById", function () {
@@ -37,7 +37,7 @@ describe("Catalogue", () => {
       expect(result).to.be.undefined;
     });
   });
-  
+
   describe("checkReorder", () => {
     it("should return an empty array when no products need reordering", function () {
       const result = cat.checkReorders();
@@ -63,8 +63,39 @@ describe("Catalogue", () => {
   });
 
 
-
-
+  describe("batchAddProducts", () => {
+    beforeEach(function () {
+      batch = {
+        type: 'Batch',
+        products: [
+          new Product("A126", "Product 6", 100, 10, 10.0),
+          new Product("A127", "Product 7", 100, 10, 10.0),
+        ],
+      };
+    });
+    it("should add products for a normal request and return the correct no. added", () => {
+      const result = cat.batchAddProducts(batch);
+      expect(result).to.equal(2);
+      let addedProduct = cat.findProductById("A126");
+      expect(addedProduct).to.not.be.undefined;
+      addedProduct = cat.findProductById("A127");
+      expect(addedProduct).to.not.be.undefined;
+    });
+    it("should only add products with a non-zero quantity in stock", () => {
+        batch.products.push(new Product("A128", "Product 8", 0, 10, 10.0));
+        const result = cat.batchAddProducts(batch);
+        expect(result).to.equal(2);
+        const rejectedProduct = cat.findProductById("A128");
+        expect(rejectedProduct).to.be.undefined;
+    });
+    it("should throw an exception when batch has a current product id", () => {
+        batch.products.push(new Product("A123", "Product 9", 0, 10, 10.0));
+        expect(() => cat.batchAddProducts(batch)).to.throw("Bad Batch");
+        // Target state
+        let rejectedProduct = cat.findProductById("A126");
+        expect(rejectedProduct).to.be.undefined; 
+    });
+  });
 
 
 });
